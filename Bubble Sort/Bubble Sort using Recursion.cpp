2e5b2b59b186de158e73812e626d4774c7b09856
Ayush gupta <91381231+Ayush150655@users.noreply.github.com>
2022-02-25 00:45:10 +0530

    Bubble Sort using Recursion

---
+++ Bubble Sort/Bubble Sort using Recursion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace  std;
+
+void bubbleSort(int *arr,int n)
+{
+    //Base Case
+    if(n==0 || n==1)
+        return ;
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+            swap(arr[i],arr[i+1]);
+    }
+    //Recursive Call
+    bubbleSort(arr,n-1);
+}
+
+int main()
+{
+    int arr[]={2,6,4,3,9,7,25,40,13};
+    bubbleSort(arr,9);
+    for(int i=0;i<9;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
